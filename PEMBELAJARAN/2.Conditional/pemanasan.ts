46114de8a9fa40663598f1703f90e07fb087fb77
Beny0895 <gunadibeny@gmail.com>
2026-01-11 16:03:11 +0700

    Pembelajaran Looping

---
+++ PEMBELAJARAN/2.Conditional/pemanasan.ts
@@ -0,0 +1,143 @@
+let nilai: number = 100;
+if(nilai >= 80 && nilai === 100 ) {
+    const pemberitahuan = "lulus"
+    console.log(pemberitahuan);
+} else if (nilai <80 && nilai >=1 ) {
+    console.log("remedial");
+} else if (nilai === 0) {
+    console.log("tidak lulus")
+} else {
+    console.log("tidak mengikuti ujian")
+}
+
+
+let value: number = 80;
+switch (true) {
+    case value >=80 : //1
+        console.log("lulus");
+    break
+    case value < 80 : //2
+        console.log("tidak lulus");
+    break
+    default: //3
+        console.log("tidak mengikuti ujian")
+}
+
+let buah: string = "apel"
+switch (buah) {
+    case "apel" :
+        const harga = "harga buah apel 10.000"
+        console.log(harga);
+    break
+    case "jeruk":
+        console.log("harga buah jeruk 8.000");
+    break
+    default: 
+        console.log("tidak beli buah");
+}
+
+
+
+// Buat program untuk mengecek apakah angka lebih besar dari 100.
+let angka1: number = 10;
+if(angka1 >= 100) {
+    console.log("angka lebih dari 100")
+} if (angka1 < 100) {
+    console.log("angka kurang dari 100")
+};
+
+// Buat program untuk mengecek apakah sebuah string kosong atau tidak.
+let kalimat: string = "";
+if(kalimat === "") {
+    console.log("string kosong");
+} if (kalimat !="") {
+    console.log("string tidak kosong")
+}
+
+// Buat program untuk mengecek apakah saldo lebih dari 1.000.000.
+let saldo: number = 10000;
+if(saldo >= 1000000) {
+    console.log("saldo lebih dari 1.000.000");
+}
+
+// Buat program untuk mengecek login berhasil atau gagal berdasarkan username.
+let username: string = "agus";
+if (username === "budi") {
+    console.log("login berhasil");
+} else {
+    console.log("login gagal")
+};
+
+// Buat program untuk mengecek nilai ujian lulus atau tidak lulus (≥ 75).
+
+let nilaiujian: number = 74;
+if(nilaiujian >= 75) {
+    console.log("lulus")
+} else {
+    console.log("tidak lulus")
+}
+
+// Buat program untuk mengecek apakah suhu panas (> 30) atau normal.
+let suhu: number = 40
+if(suhu >=30) {
+    console.log("suhu panas");
+} else {
+    console.log("suhu tidak panas")
+};
+
+// Buat program yang menampilkan jenis kendaraan berdasarkan kode (M, C, B).
+
+let kendaraan: string = "avanza";
+switch (kendaraan) {
+    case "avanza" :
+        console.log("kode M");
+    break;
+    case "ferari" :
+        console.log("kode C");
+    break
+    case "bmw" :
+        console.log("kode B");
+}
+
+// Buat program untuk menampilkan bulan berdasarkan angka 1–12.
+let angka: number = 10;
+switch (angka) {
+    case 1 :
+        console.log("january"); break;
+    case 2 :
+        console.log("february");break;
+    case 3 :
+        console.log("maret");break;
+    case 4 :
+        console.log("april"); break;
+    case 5 :
+        console.log("mei");break;
+    case 6 :
+        console.log("juni");break;
+    case 7 :
+        console.log("juli");break;
+    case 8 :
+        console.log("agustus");break;
+    case 9 :
+        console.log("september");break;
+    case 10 :
+        console.log("oktober");break;
+    case 11 :
+        console.log("november");break;
+    case 12 : 
+    console.log("desember");break;
+};
+
+// Buat program yang menampilkan status pembayaran (lunas, pending, batal).
+let status1: string = "lunas";
+switch (status1) {
+    case "lunas" :
+        console.log("lunas");
+    break;
+    case "pending" :
+        console.log("pending");
+    break;
+    case "batal" :
+        console.log("batal");
+};
+
---
+++ PEMBELAJARAN/3.Looping/index.ts
@@ -0,0 +1,66 @@
+// no 1
+// Buat program menggunakan for untuk menampilkan angka 1 sampai 5 ke console.
+for (let a :number = 0; a <=5; a++) {
+    console.log(a);
+};
+
+// no 2
+// Gunakan for untuk menampilkan angka genap dari 2 sampai 10.
+for(let b : number = 0; b < 10; b+=2 ) {
+    console.log(b)
+};
+
+// no 3
+// Buat for loop yang menampilkan teks.
+for (let c: number = 0; c <= 3; c++) {
+    console.log("belajar javascript")
+};
+
+// no 4
+// Gunakan while untuk menampilkan angka 1 sampai 5.
+let d: number = 0;
+while (d <= 5) {
+    console.log(d);
+    d++
+};
+
+// no 5
+// Buat program while yang menampilkan angka 5 sampai 1 (mundur).
+let e:number = 5;
+while(e >= 1) {
+    console.log(e)
+    e--
+};
+
+// no 6
+// Gunakan while untuk menampilkan angka ganjil dari 1 sampai 9.
+let f: number = 9;
+while(f > 0) {
+    console.log(f)
+    f -= 2
+};
+
+// no 7
+// Buat program do...while yang menampilkan angka 1 sampai 5.
+let g: number = 0;
+do {
+    g++
+    console.log(g)
+} while (g <= 5);
+
+// no 8
+// Gunakan do...while untuk menampilkan teks "Halo Dunia" sebanyak 3 kali.
+let h: number = 0;
+do{
+    h++
+    console.log("Hallo Dunia");
+} while (h <= 3);
+
+// no 9
+// Buat do...while untuk menampilkan angka 10, 8, 6, 4, 2.
+let i: number = 10;
+do{
+    console.log(i);
+    i-=2
+    // console.log(i);
+} while (i >= 2)
