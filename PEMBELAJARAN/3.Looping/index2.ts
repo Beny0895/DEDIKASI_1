905656275bcd39b977809b257f5c7c8962a8b6e2
Beny0895 <gunadibeny@gmail.com>
2026-01-12 13:54:22 +0700

    feat: hari 2 looping

---
+++ PEMBELAJARAN/3.Looping/index2.ts
@@ -0,0 +1,71 @@
+// SENIN, 12, JANUARY 2026, 13.02 - 13.44
+
+// no 1 
+// Buat program untuk menampilkan angka 1 sampai 10 ke console.
+for(let a: number = 1;a <= 10; a++) {
+    console.log(a);
+};
+
+// no 2
+// Buat program untuk menampilkan angka 20 sampai 2, berkurang 2 setiap perulangan.
+for(let b: number = 20; b >= 2; b-=2){
+    console.log(b);
+};
+
+// no 3
+// Buat program untuk menampilkan bilangan ganjil dari 1 sampai 15.
+for(let c: number = 1; c <= 15; c+=2) {
+    console.log(c);
+};
+
+// no 4
+// Buat program untuk menampilkan angka 1 sampai 5 menggunakan while.
+let d: number = 1;
+while (d <= 5) {
+    console.log(d);
+    d++
+};
+
+// no 5
+// Buat program untuk menampilkan angka 10 sampai 0 dengan pengurangan 1.
+let e: number = 10;
+while(e >= 0) {
+    console.log(e);
+    e--
+};
+
+// no 6
+// Buat program untuk menampilkan angka kelipatan 3 dari 3 sampai 30.
+let f: number = 3;
+while(f <= 30) {
+    console.log(f);
+    f+=3
+};
+
+// no 7
+// Buat program untuk menampilkan angka 1 sampai 5 menggunakan do...while.
+let g: number = 1;
+do {
+    console.log(g); //yg ini dicek dulu baru dijalankan
+    g++
+    console.log(g); //yg ini dijalankan dlu baru di cek
+} while (g <= 5);
+
+// no 8
+// Buat program untuk menampilkan angka 10, 8, 6, 4, 2 menggunakan do...while.
+let h: number = 10;
+do{
+    console.log(h)
+    h -= 2
+} while (h >= 2);
+
+// no 9
+// uat program untuk menampilkan angka 5 sampai -5 menggunakan do...while.
+let i: number = 5;
+do {
+    console.log(i)
+    i--
+} while (i >= -5);
+
+
+
